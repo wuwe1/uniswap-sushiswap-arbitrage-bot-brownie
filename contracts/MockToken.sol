acec22bcdf9506005d496740005bf73cda8ada92
wuwe1 <wu_we1@outlook.com>
2022-04-03 16:56:44 -0400

    ⚡️ use ERC20PresetFixedSupply

--- contracts/MockToken.sol
+++ contracts/MockToken.sol
@@ -1,10 +1,12 @@
 pragma solidity 0.8.10;
 
-import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
+import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
 
-contract MockToken is ERC20PresetMinterPauser {
-    constructor(string memory name, string memory symbol)
-        public
-        ERC20PresetMinterPauser(name, symbol)
-    {}
+contract MockToken is ERC20PresetFixedSupply {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply,
+        address owner
+    ) public ERC20PresetFixedSupply(name, symbol, initialSupply, owner) {}
 }
