3484503a4e0a3798661efc724f78d434c365ff4d
wuwe1 <wu_we1@outlook.com>
2022-04-08 00:43:27 -0400

    💡 more comments on uniswapv2 utils

--- scripts/utils/uniswapv2.py
+++ scripts/utils/uniswapv2.py
@@ -8,10 +8,16 @@ def compute_profit_maximizing_trade(
     s_a: int, s_b: int, u_a: int, u_b: int
 ) -> Tuple[bool, int]:
     """
+    a -> b:
             _____________
           ╲╱ sa⋅sb⋅ua⋅ub
     -ua + ───────────────
                 sb
+    b -> a:
+            _____________
+          ╲╱ sa⋅sb⋅ua⋅ub
+    -ub + ───────────────
+                sa
     """
     a_to_b = u_a / u_b < s_a / s_b
     invariant = u_a * u_b
@@ -32,6 +38,14 @@ def div(a, b) -> int:
 
 
 def get_amount_out(amount_in, reserve_in, reserve_out) -> int:
+    """
+    amount_in: x
+    reserve_in: in
+    reserve_out: out
+    0.997⋅out⋅x
+    ───────────
+    in + 0.997⋅x
+    """
     amount_in_with_fee = amount_in * 997
     n = amount_in_with_fee * reserve_out
     d = reserve_in * 1000 + amount_in_with_fee
@@ -39,6 +53,14 @@ def get_amount_out(amount_in, reserve_in, reserve_out) -> int:
 
 
 def get_amount_in(amount_out, reserve_in, reserve_out) -> int:
+    """
+    amount_out: y
+    reserve_in: in
+    reserve_out: out
+    1000⋅in⋅y
+    ───────────
+    997⋅(out - y)
+    """
     n = reserve_in * amount_out * 1000
     d = (reserve_out - amount_out) * 997
     return div(n, d) + 1
